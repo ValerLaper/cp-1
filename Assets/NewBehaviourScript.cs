4b41c728a7105e92af949a3b9f0d919909263138
ValerLaper <67782667+ValerLaper@users.noreply.github.com>
2020-07-03 10:35:03 +0100

    Add files via upload

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+      public float speed = 1;
+
+    void Start() {
+        // Initial Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+}
